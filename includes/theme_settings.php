e0f43ba88e9affc43526b0dfd2350baa0bf08eca
ImAadarsh <aadarshkavita@gmail.com>
2025-04-20 13:55:21 +0530

    AA

--- includes/theme_settings.php
+++ includes/theme_settings.php
@@ -266,7 +266,6 @@
 
         <div class="d-flex align-items-center gap-2 px-3 py-2 offcanvas-header border-top border-dashed">
             <button type="button" class="btn w-50 btn-soft-danger" id="reset-layout">Reset</button>
-            <button type="button" class="btn w-50 btn-soft-info">Buy Now</button>
         </div>
 
     </div>
